149e3166ca6f054490c5f3433e69f89ed1f45295
Kamie Short <kamie.short@gmail.com>
2022-03-31 15:36:43 -0700

    logout is working with conditional rendering of sign-in and logout buttons

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { logout } from '../services/users';
+import './header.css';
 
-export default function Header() {
-  return <div>header</div>;
+export default function Header({ currentUser, setCurrentUser }) {
+  const logoutClick = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
+  return (
+    <div className="header ">
+      <NavLink className="nav" exact to="/">
+        Home
+      </NavLink>
+
+      {currentUser ? (
+        <button className="nav" onClick={logoutClick}>
+          Logout
+        </button>
+      ) : (
+        <NavLink className="nav" exact to="/auth">
+          Sign-In
+        </NavLink>
+      )}
+    </div>
+  );
 }
